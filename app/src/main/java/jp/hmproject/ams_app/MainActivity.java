82f2f811227a5bfcec87e162853c6303bd05e4f3
HM <himiz6912@gmail.com>
2016-12-18 20:09:10 +0700

    timepicker_dialog

--- app/src/main/java/jp/hmproject/ams_app/MainActivity.java
+++ app/src/main/java/jp/hmproject/ams_app/MainActivity.java
@@ -1,5 +1,7 @@
 package jp.hmproject.ams_app;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context = this.context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final Button start_bt = (Button)findViewById(R.id.start);
+        start_bt.setOnClickListener((new View.OnClickListener(){
+
+            Calendar cal = Calendar.getInstance();
+            int hour = cal.get(Calendar.HOUR_OF_DAY);
+            int minute = cal.get(Calendar.MINUTE);
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog tpd = new TimePickerDialog(
+                        getApplicationContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        start_bt.setText(Integer.toString(i) + ":" + Integer.toString(i1));
+                    }
+                },hour,minute,false);
+            }
+        }));
     }
 
     @Override
